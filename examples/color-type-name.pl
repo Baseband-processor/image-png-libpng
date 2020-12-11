5de26368039d2ad88bbdd87d6d549b9ddc2c59c8
Edoardo Mantovani <Baseband@cpan.org>
2020-12-11 12:00:34 +0100

    Small modifications...
    Hi,
    I've noticed that every example file has wrong shellbags, this won't help the interpreter to execute the perl code.
    
    I've seen that also the code refers to an image, I think that it could be better if the image is given in @ARGV.
    
    also note that the BEGIN block helps us to don't load libraries if the file inserted does not exists. ( this will save additional memory)
    
    Regards
    Edoardo Mantovani, 2020

--- examples/color-type-name.pl
+++ examples/color-type-name.pl
@@ -1,7 +1,22 @@
-#!/home/ben/software/install/bin/perl
+#!/usr/bin/perl
+
+# Modified by Edoardo Mantovani, 2020
+
+sub BEGIN{
+
+my $image = shift || die "please insert image!\n";
+if( undef( $image ) || (! -e $image ) ){
+    die "insert a valid image!\n";
+  }
+}
+
+sub END{
+
 use warnings;
 use strict;
 use Image::PNG::Libpng ':all';
-my $png = read_png_file ('tantei-san.png');
+my $png = read_png_file ( $image );
 my $name = color_type_name ($png->get_IHDR->{color_type});
 print "Your PNG has colour type $name.\n";
+
+}
