85910fdd70a1f2e919cb77ffdf7ecd3e27842527
Edoardo Mantovani <Baseband@cpan.org>
2020-12-11 11:55:12 +0100

    Update build.pl

--- build.pl
+++ build.pl
@@ -1,9 +1,14 @@
-#!/home/ben/software/install/bin/perl
+#!/usr/bin/perl
+
+sub BEGIN{
+
 use warnings;
 use strict;
 use Perl::Build;
 use FindBin '$Bin';
 perl_build (
     makefile => 'makeitfile',
-    pre => "/home/ben/projects/check4libpng/copy2inc.pl $Bin/inc",
+    pre => "/home/ben/projects/check4libpng/copy2inc.pl $Bin/inc", # ??
 );
+
+}
